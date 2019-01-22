7ac404a7f0a088aa0a1c59783dceb61e9002f876
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-22 18:02:08 -0500

    MVP. Completed main portain of closure.js.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,34 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const myLilClosure = (noun, adj, cb) => {
+  cb(noun, adj);
+};
 
+const very = (noun, adj) => {
+  console.log(`I think ${noun} is very ${adj}.`);
+};
+
+myLilClosure("coding", "fun", very);
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return (uptick = () => {
+    console.log((counter += 1));
+  });
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+const newerCounter = counter();
+newCounter(); // expect 1
+newCounter(); // expect 2
+newCounter(); // expect 3
+newerCounter(); // expect 1
+newerCounter(); // expect 2
+newCounter(); // expect 4
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
