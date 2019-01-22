bd0b49de08dc37a3774b9fa028b7dacabd7771f6
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-22 18:28:49 -0500

    Stretch: completed stretch portion of closure.js.

--- assignments/closure.js
+++ assignments/closure.js
@@ -22,19 +22,45 @@ const counter = () => {
 // newCounter(); // 1
 // newCounter(); // 2
 const newCounter = counter();
-const newerCounter = counter();
+const newerCounter = counter(); // Shows that multple counters can be created
 newCounter(); // expect 1
 newCounter(); // expect 2
 newCounter(); // expect 3
-newerCounter(); // expect 1
+newerCounter(); // expect 1 - Shows that each created counter is independent of any others
 newerCounter(); // expect 2
 newCounter(); // expect 4
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  return {
+    counter: 0,
+    // I Included a repeat parameter with a default of 1 for the inc and dec methods.
+    inc: function(repeat = 1) {
+      for (i = 0; i < repeat; i++) {
+        this.counter += 1;
+      }
+      return this.counter;
+    },
+    dec: function(repeat = 1) {
+      for (i = 0; i < repeat; i++) {
+        this.counter -= 1;
+      }
+      return this.counter;
+    }
+  };
 };
+
+const lovelyCounter = counterFactory();
+
+console.log(lovelyCounter); // expect lovelyCounter object
+console.log(lovelyCounter.counter); // expect 0
+lovelyCounter.dec();
+lovelyCounter.dec(2); // Used an argument of 2 for repeat parameter to decrement 2 times.
+console.log(lovelyCounter.counter); // expect -3
+lovelyCounter.inc(5);
+console.log(lovelyCounter.counter); // expect 2
